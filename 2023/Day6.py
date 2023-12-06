79a32f49f96707489fdd795210a97fba1b0cc29c
Aman Chhabra <amanchhabra1990@gmail.com>
2023-12-06 07:50:48 +0100

    Advent of Code Day 6

---
+++ 2023/Day6.py
@@ -0,0 +1,53 @@
+import re
+
+# Puzzle 1
+
+def solvePuzzle1() :
+    count = 0
+    line = input()
+    times = [int(time) for time in re.findall('\d+', line)]
+
+    line = input()
+    distances = [int(distance) for distance in re.findall('\d+', line)]
+
+    for time in range(len(times)):
+        ways = 0
+        speed = 0
+        for second in range(times[time]):
+            if(speed*(times[time]-second))>distances[time]:
+                ways = ways+1
+            speed = speed + 1
+        if count == 0:
+            count = ways
+        else:
+            count = count * ways
+    print(count)
+
+
+
+
+
+
+# Puzzle 2
+
+def solvePuzzle2() :
+    count = 0
+    line = input()
+    times = [int("".join(re.findall('\d+', line)))]
+
+    line = input()
+    distances = [int("".join(re.findall('\d+', line)))]
+    for time in range(len(times)):
+        ways = 0
+        speed = 0
+        for second in range(times[time]):
+            if(speed*(times[time]-second))>distances[time]:
+                ways = ways+1
+            speed = speed + 1
+        if count == 0:
+            count = ways
+        else:
+            count = count * ways
+    print(count)
+
+solvePuzzle2()
